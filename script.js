302caca3a3b8560f1a045b97177d5eb6c08fa5fd
Yaswanth <byaswanth2003@gmail.com>
2023-06-16 14:02:26 +0530

    Add Functionality - Add Grid creation deletion - Add Dynamic Grid Size - Use Value of input type range for grid Size - Add Fixed Width - Add Dynamic Square Size - Add Dynamic No of Squares [Ticket: 08]

--- script.js
+++ script.js
@@ -1,69 +1,98 @@
-const container = document.querySelector(".container")
-const drawingArea = document.querySelector(".drawing-area")
-const buttons = document.querySelectorAll("button") 
+const container = document.querySelector(".container");
+const drawingArea = document.querySelector(".drawing-area");
+const buttons = document.querySelectorAll("button");
+const sizeSpans = document.querySelectorAll(".size-span");
 
-buttons.forEach(button => button.addEventListener('click', () => {
-    if(button.id !=="clear"){
-        buttons.forEach(btn => btn.classList.remove("active"));
-        button.classList.add("active");
+let gridSize = 16;
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (button.id !== "clear") {
+      buttons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
     }
-  }));
-const colorPicker = document.getElementById("color-picker")
+  })
+);
 
+const colorPicker = document.getElementById("color-picker");
 
-for (let x = 0; x < 40; x++) {
-    const square = document.createElement("div")
-    square.classList.add("square")
-    drawingArea.appendChild(square)
+function createGrid() {
+  drawingArea.innerHTML = "";
+
+  for (let x = 0; x < gridSize; x++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    drawingArea.appendChild(row);
+    for (let y = 0; y < gridSize; y++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+  }
+
+  let gridWidth = container.offsetWidth / gridSize;
+
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.style.width = `${gridWidth}px`;
+    square.style.height = `${gridWidth}px`;
+  });
+
+  colorPicker.addEventListener("input", () => {
+    const selectedColor = colorPicker.value;
+    colormode(selectedColor);
+  });
+  sizeSpans.forEach((size) => (size.textContent = gridSize));
 }
-for (let y = 0; y < 37; y++) {
-    const clone = drawingArea.cloneNode(true)
-    container.appendChild(clone)   
-}
-const squares = document.querySelectorAll(".square")
-colorPicker.addEventListener("input",()=>{
-    const selectedColor = colorPicker.value
-    colormode(selectedColor)  
-})
 
+createGrid();
 
-const clear = document.getElementById("clear")
+const clear = document.getElementById("clear");
 
-clear.addEventListener('click',()=> {
-    squares.forEach(square => square.style.backgroundColor = "white")
-})
+clear.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => (square.style.backgroundColor = "white"));
+});
 
-const inputRange = document.getElementById("size-range")
-const sizeSpans = document.querySelectorAll(".size-span")
+const inputRange = document.getElementById("size-range");
 
-inputRange.addEventListener("input",()=>{
-    const rangeValue = inputRange.value
-    sizeSpans.forEach(size => size.textContent = rangeValue)
-})
+inputRange.addEventListener("input", () => {
+  gridSize = inputRange.value;
+  createGrid();
+});
 
-const eraser = document.getElementById("eraser")
+const eraser = document.getElementById("eraser");
 
-eraser.addEventListener('click',()=>{
-    squares.forEach(square => square.addEventListener('mouseover',()=> {
-        square.style.backgroundColor = "#fff" 
-})) 
-})
+eraser.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#fff";
+    });
+  });
+});
 
+const rainbowMode = document.getElementById("rainbow-mode");
 
-const rainbowMode = document.getElementById("rainbow-mode")
+rainbowMode.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = `#${Math.floor(
+        Math.random() * 16777215
+      ).toString(16)}`;
+    });
+  });
+});
 
-rainbowMode.addEventListener('click',()=>{
-    squares.forEach(square => square.addEventListener('mouseover',()=>{
-        square.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`
-
-    }))
-})
-
-const colormode = (selectedColor)=>{
-    const colorMode = document.getElementById("color-mode")
-    colorMode.addEventListener('click',()=>{
-        squares.forEach(square => square.addEventListener('mouseover',()=>{
-            square.style.backgroundColor = selectedColor
-        }))
-    })
-}
+const colormode = (selectedColor) => {
+  const colorMode = document.getElementById("color-mode");
+  colorMode.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = selectedColor;
+      });
+    });
+  });
+};
